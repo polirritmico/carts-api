7d219454267d8962a5846ef46dbd8149ff9a87af
polirritmico <ebray187@gmail.com>
2026-05-21 14:19:04 -0400

    format: remove commented code

--- src/main/java/cl/duoc/carts/mapper/DtoModelMapper.java
+++ src/main/java/cl/duoc/carts/mapper/DtoModelMapper.java
@@ -6,37 +6,37 @@
  */
 package cl.duoc.carts.mapper;
 
-import cl.duoc.carts.dto.request.CartCreationRequest;
-import cl.duoc.carts.dto.request.CartUpdateRequest;
 import cl.duoc.carts.dto.response.CartResponse;
 import cl.duoc.carts.model.Cart;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class DtoModelMapper {
+
     public CartResponse toCartResponse(Cart cart) {
         return CartResponse.builder()
                 .id(cart.getId())
+                .customer(cart.getCustomer())
+                .status(cart.getStatus().name())
                 .createdAt(cart.getCreatedAt())
                 .updatedAt(cart.getUpdatedAt())
-                .deletedAt(cart.getDeletedAt())
                 .build();
     }
 
-    public Cart cartFromCreationRequest(CartCreationRequest req) {
-        return Cart.builder()
-                .customerId(req.getCustomerId())
-                .createdAt(LocalDateTime.now())
-                .build();
-    }
+    // public Cart cartFromCreationRequest(CartCreationRequest req) {
+    //     return Cart.builder()
+    //             .customerId(req.getCustomerId())
+    //             .createdAt(LocalDateTime.now())
+    //             .build();
+    // }
 
-    public Cart saleFromUpdateRequest(CartUpdateRequest req) {
-        return Cart.builder()
-                .customerId(req.getCustomerId())
-                .createdAt(LocalDateTime.now())
-                .build();
-    }
+    // public Cart saleFromUpdateRequest(CartUpdateRequest req) {
+    //     return Cart.builder()
+    //             .customerId(req.getCustomerId())
+    //             .createdAt(LocalDateTime.now())
+    //             .build();
+    // }
+
 }
--- src/main/java/cl/duoc/carts/service/CartService.java
+++ src/main/java/cl/duoc/carts/service/CartService.java
@@ -6,20 +6,12 @@
  */
 package cl.duoc.carts.service;
 
-import cl.duoc.carts.dto.request.CartCreationRequest;
-import cl.duoc.carts.dto.request.CartUpdateRequest;
 import cl.duoc.carts.dto.response.CartResponse;
-import cl.duoc.carts.exception.CartNotFoundException;
 import cl.duoc.carts.mapper.DtoModelMapper;
-import cl.duoc.carts.model.Cart;
 import cl.duoc.carts.repository.CartRepository;
-import jakarta.transaction.Transactional;
-import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,31 +22,7 @@ public class CartService {
 
     private final DtoModelMapper mapper;
 
-    public CartResponse findById(Long id) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        log.info("Starting findById with id: " + id + " by user: " + auth.getName());
-        return cartRepo.findById(id).map(mapper::toCartResponse).orElseThrow(() -> new CartNotFoundException(id));
-    }
-
     public List<CartResponse> findAll() {
-        return cartRepo.findAllActive().stream().map(mapper::toCartResponse).toList();
-    }
-
-    public CartResponse saveCart(CartCreationRequest req) {
-        Cart newCart = cartRepo.save(mapper.cartFromCreationRequest(req));
-        return mapper.toCartResponse(newCart);
-    }
-
-    @Transactional
-    public CartResponse replaceCart(Long id, CartUpdateRequest req) {
-        Cart updatedCart = cartRepo.findById(id).orElseThrow(() -> new CartNotFoundException(id));
-        updatedCart.setCustomerId(req.getCustomerId());
-        updatedCart.setUpdatedAt(LocalDateTime.now());
-        return mapper.toCartResponse(updatedCart);
-    }
-
-    @Transactional
-    public void deleteCart(Long id) {
-        cartRepo.findById(id).orElseThrow(() -> new CartNotFoundException(id)).setDeletedAt(LocalDateTime.now());
+        return cartRepo.findAll().stream().map(mapper::toCartResponse).toList();
     }
 }
