da32a41147705c4decf3e65640d5d6308f8552b5
polirritmico <ebray187@gmail.com>
2026-05-21 14:18:02 -0400

    feat(CartRepository): clean the repository

--- src/main/java/cl/duoc/carts/repository/CartRepository.java
+++ src/main/java/cl/duoc/carts/repository/CartRepository.java
@@ -7,15 +7,8 @@
 package cl.duoc.carts.repository;
 
 import cl.duoc.carts.model.Cart;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    List<Cart> findAllByDeletedAtIsNull();
-
-    default List<Cart> findAllActive() {
-        return findAllByDeletedAtIsNull();
-    }
-}
+public interface CartRepository extends JpaRepository<Cart, Long> {}
