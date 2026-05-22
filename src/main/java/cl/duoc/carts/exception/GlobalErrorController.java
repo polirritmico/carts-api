4dfcf7e44e50b0ca5345dc4a2db1e72f32d3d106
polirritmico <ebray187@gmail.com>
2026-05-22 11:32:09 -0400

    feat: add GlobalErrorController handler for bad endpoints reqs

---
+++ src/main/java/cl/duoc/carts/exception/GlobalErrorController.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.carts.exception;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.webmvc.error.ErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Hidden
+public class GlobalErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public ResponseEntity<Void> handleError(HttpServletRequest req, HttpServletResponse res) {
+        log.error(
+                "Container error - Status: {}, Path: {} | Message: {}",
+                res.getStatus(),
+                req.getAttribute(RequestDispatcher.ERROR_REQUEST_URI),
+                req.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        return ResponseEntity.status(res.getStatus()).build();
+    }
+}
