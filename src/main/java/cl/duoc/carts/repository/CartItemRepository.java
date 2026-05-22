89591ded52a6118d2ce423b6797b56cf0ffc6307
polirritmico <ebray187@gmail.com>
2026-05-22 04:46:08 -0400

    feat: add CartItemRepository repository

---
+++ src/main/java/cl/duoc/carts/repository/CartItemRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.carts.repository;
+
+import cl.duoc.carts.model.CartItem;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    List<CartItem> findByCartId(Long cartId);
+
+    void deleteByCartId(Long cartId);
+}
