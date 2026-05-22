eb2995c2fe1f8b678ebe353172e4f815528fef51
polirritmico <ebray187@gmail.com>
2026-05-22 04:42:45 -0400

    feat: add CartItemResponse DTO

---
+++ src/main/java/cl/duoc/carts/dto/response/CartItemResponse.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.carts.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemResponse {
+    private Long id;
+    private Long product;
+    private Integer quantity;
+    private Integer price;
+}
