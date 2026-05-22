9770d5453b8a27e229cd21fe62c99dbd969a1deb
polirritmico <ebray187@gmail.com>
2026-05-22 11:17:44 -0400

    feat(CartItemNotFoundException): add domain exception

---
+++ src/main/java/cl/duoc/carts/exception/CartItemNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.carts.exception;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CartItemNotFoundException extends RuntimeException {
+    public CartItemNotFoundException(Long cartId, Long itemId) {
+        String msg = "Cart item with id '" + itemId + "' in cart '" + cartId + "' not found in the DB.";
+        log.error(msg);
+        super(msg);
+    }
+}
