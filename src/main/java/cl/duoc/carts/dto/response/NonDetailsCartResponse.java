1076a66f4c705400946be1022aaad6261cfbe865
polirritmico <ebray187@gmail.com>
2026-05-22 04:43:51 -0400

    feat: add NonDetailsCartResponse DTO

---
+++ src/main/java/cl/duoc/carts/dto/response/NonDetailsCartResponse.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.carts.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NonDetailsCartResponse {
+    private Long id;
+    private Long customer;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
